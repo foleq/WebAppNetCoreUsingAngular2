2ff0f9a0588d5248779bf295b80b8095665269a5
foleq <piotrekgda90@gmail.com>
2017-04-16 17:11:23 +0200

    L04S03 - Two-way binding - easier way using FormsModule

--- src/WebAppNetCoreUsingAngular2/wwwroot/app/app.module.ts
+++ src/WebAppNetCoreUsingAngular2/wwwroot/app/app.module.ts
@@ -1,11 +1,12 @@
 ﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; // Module needed for running Angular websites
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { CarPartsComponent } from './car-parts.component';
 
-
 @NgModule({
-    imports: [BrowserModule], // Loads required dependencies to launch our app in the browser
+    imports: [BrowserModule, FormsModule], // Loads required dependencies to launch our app in the browser
     declarations: [
         AppComponent,
         CarPartsComponent
