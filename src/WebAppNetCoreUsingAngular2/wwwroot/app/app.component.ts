531a39543b9806582e416275833dc6cc204c8a18
foleq <piotrekgda90@gmail.com>
2017-04-16 14:55:38 +0200

    L02S02 - Pipes & Methods

--- src/WebAppNetCoreUsingAngular2/wwwroot/app/app.component.ts
+++ src/WebAppNetCoreUsingAngular2/wwwroot/app/app.component.ts
@@ -3,11 +3,12 @@
 @Component({
     selector: 'my-app',
     template: `<h1>{{title}}</h1>
+        <p>There are {{totalCarParts()}} total parts in stock.</p>
         <ul>
             <li *ngFor="let carPart of carParts">
-                <h2>{{carPart.name}}</h2> 
+                <h2>{{carPart.name | uppercase}}</h2> 
                 <p>{{carPart.description}}</p> 
-                <p *ngIf="carPart.inStock > 0">{{carPart.inStock}} in Stock</p> 
+                <p *ngIf="carPart.inStock > 0">{{carPart.inStock | currency: 'EUR': true}} in Stock</p> 
                 <p *ngIf="carPart.inStock === 0">Out of Stock</p>
             </li>
         </ul>`
@@ -19,18 +20,30 @@ export class AppComponent {
         "id": 1,
         "name": "Super Tires",
         "description": "These tires are the very best",
-        "inStock": 5
+        "inStock": 5,
+        "price": 4.99
     },
     {
         "id": 2,
         "name": "Reinforced Shocks",
         "description": "Shocks made from kryptonite",
-        "inStock": 4
+        "inStock": 4,
+        "price": 9.99
     },
     {
         "id": 3,
         "name": "Test",
         "description": "Test description",
-        "inStock": 0
+        "inStock": 0,
+        "price": 3.99
     }];
+
+    totalCarParts() {
+        let sum = 0;
+        for (let carPart of this.carParts) {
+            sum += carPart.inStock;
+        }
+        return sum;
+        // return this.carParts.reduce((prev, current) => prev + current.inStock, 0); // Simplifer
+    }
 }
