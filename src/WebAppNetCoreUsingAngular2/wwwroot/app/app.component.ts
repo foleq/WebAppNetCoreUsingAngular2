dd65b6991c137308762bce282295ec93b0f8b32f
foleq <piotrekgda90@gmail.com>
2017-04-16 14:43:02 +0200

    L01S01 - Added structural directives - *ngFor and *ngIf

--- src/WebAppNetCoreUsingAngular2/wwwroot/app/app.component.ts
+++ src/WebAppNetCoreUsingAngular2/wwwroot/app/app.component.ts
@@ -3,17 +3,34 @@
 @Component({
     selector: 'my-app',
     template: `<h1>{{title}}</h1>
-        <h2>{{carPart.name}}<h2> 
-        <p>{{carPart.description}}<p> 
-        <p>{{carPart.inStock}} in Stock<p>`
+        <ul>
+            <li *ngFor="let carPart of carParts">
+                <h2>{{carPart.name}}</h2> 
+                <p>{{carPart.description}}</p> 
+                <p *ngIf="carPart.inStock > 0">{{carPart.inStock}} in Stock</p> 
+                <p *ngIf="carPart.inStock === 0">Out of Stock</p>
+            </li>
+        </ul>`
 })
 export class AppComponent {
 
     title = 'Ultra Racing';
-    carPart = {
+    carParts = [{
         "id": 1,
         "name": "Super Tires",
         "description": "These tires are the very best",
         "inStock": 5
-    };
+    },
+    {
+        "id": 2,
+        "name": "Reinforced Shocks",
+        "description": "Shocks made from kryptonite",
+        "inStock": 4
+    },
+    {
+        "id": 3,
+        "name": "Test",
+        "description": "Test description",
+        "inStock": 0
+    }];
 }
