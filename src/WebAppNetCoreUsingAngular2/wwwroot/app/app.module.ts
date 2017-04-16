91fcdf630a5db472e20d69f0635edbdadc29b1e0
foleq <piotrekgda90@gmail.com>
2017-04-16 15:28:54 +0200

    L03S01 - Splitting to App and CarParts components

--- src/WebAppNetCoreUsingAngular2/wwwroot/app/app.module.ts
+++ src/WebAppNetCoreUsingAngular2/wwwroot/app/app.module.ts
@@ -1,10 +1,15 @@
 ﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; // Module needed for running Angular websites
 import { AppComponent } from './app.component';
+import { CarPartsComponent } from './car.parts.component';
+
 
 @NgModule({
     imports: [BrowserModule], // Loads required dependencies to launch our app in the browser
-    declarations: [AppComponent],
+    declarations: [
+        AppComponent,
+        CarPartsComponent
+    ],
     bootstrap: [AppComponent] // Indicates our root component
 })
 export class AppModule { }
---
+++ src/WebAppNetCoreUsingAngular2/wwwroot/app/car.parts.component.ts
@@ -0,0 +1,47 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'car-parts',
+    template: `<p>There are {{totalCarParts()}} total parts in stock.</p>
+        <ul>
+            <li *ngFor="let carPart of carParts">
+                <h2>{{carPart.name | uppercase}}</h2> 
+                <p>{{carPart.description}}</p> 
+                <p *ngIf="carPart.inStock > 0">{{carPart.inStock | currency: 'EUR': true}} in Stock</p> 
+                <p *ngIf="carPart.inStock === 0">Out of Stock</p>
+            </li>
+        </ul>`
+})
+export class CarPartsComponent {
+
+    carParts = [{
+        "id": 1,
+        "name": "Super Tires",
+        "description": "These tires are the very best",
+        "inStock": 5,
+        "price": 4.99
+    },
+    {
+        "id": 2,
+        "name": "Reinforced Shocks",
+        "description": "Shocks made from kryptonite",
+        "inStock": 4,
+        "price": 9.99
+    },
+    {
+        "id": 3,
+        "name": "Test",
+        "description": "Test description",
+        "inStock": 0,
+        "price": 3.99
+    }];
+
+    totalCarParts() {
+        let sum = 0;
+        for (let carPart of this.carParts) {
+            sum += carPart.inStock;
+        }
+        return sum;
+        // return this.carParts.reduce((prev, current) => prev + current.inStock, 0); // Simplifer
+    }
+}
